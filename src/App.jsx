72dc5129b3246245faa73daf36228f9404a76afc
Horlakz <horlakz@proton.me>
2022-06-02 18:46:34 +0100

    renamed files ext to .jsx

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+//import {CssBaseline, Grid} from '@material-ui/core';
+//import Header from "./components/header/header";
+//import List from "./components/header/list/list";
+//import Map from "./components/header/map/map";
+
+const App = () => {
+  return (
+    <>
+      {/* <CssBaseline />
+            <Header />
+            <Grid container spacing={3} style={{width:'100%'}}>
+                <Grid item xs={12} md={4}>
+                    <list />
+
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <map />
+                    
+                </Grid>
+            </ Grid> */}
+      hello world
+    </>
+  );
+};
+export default App;
+
+/*
+
+            
+            
+*/
